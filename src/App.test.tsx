abe856a746955140b1d6907202dce5e4615c779f
akram rasikh <akram.rasikh@gmail.com>
2021-04-20 23:44:42 +0100

    test for filter implemented. feature yet to be

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, within } from '@testing-library/react';
+import { fireEvent, render, screen, within } from '@testing-library/react';
 import App from './App';
 import { getMovies } from './services/movie-apis';
 import { mockMovieResults } from './mock-data';
@@ -16,6 +16,22 @@ test('shows all movies rated from 5 stars down to 1 by default', async () => {
   expect(within(bestRated).getByText(mockMovieResults[15].title)).toBeDefined();
   expect(within(worstRated).getByText(mockMovieResults[2].title)).toBeDefined();
 });
-// test('filters through movie items', () => {});
+
+test('filters through movie items', async () => {
+  getMovies.mockImplementation(() => mockMovieResults);
+  render(<App />);
+  await screen.findByText(mockMovieResults[0].title);
+  expect(screen.getAllByTestId('movie-id').length).toBe(20);
+  const checkbox8 = screen.getByTestId('vote-score-8');
+  fireEvent.click(checkbox8);
+  expect(screen.getByText('No movies found')).toBeDefined();
+  expect(screen.queryAllByTestId('movie-id').length).toBe(0);
+  const checkbox7 = screen.getByTestId('vote-score-7');
+  fireEvent.click(checkbox7);
+  expect(screen.getAllByTestId('movie-id')).toBe(8);
+  const checkbox6 = screen.getByTestId('vote-score-6');
+  fireEvent.click(checkbox6);
+  expect(screen.getAllByTestId('movie-id')).toBe(16);
+});
 // test('filtered movie items ordered by rating within sublevel too', () => {});
 // test('shows message "no X star films" if none available', () => {});
