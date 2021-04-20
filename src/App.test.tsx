af3a9f10e70685058136a6c78e2ef3fd4c3a8ab4
akram rasikh <akram.rasikh@gmail.com>
2021-04-20 21:19:49 +0100

    nominal test cases written

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,11 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/filter app/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('shows movies rated from 5 stars down to 1 by default', () => {});
+test('filters through movie items', () => {});
+test('filtered movie items ordered by rating within sublevel too', () => {});
+test('shows message "no X star films" if none available', () => {});
